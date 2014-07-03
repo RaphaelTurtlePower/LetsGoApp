225fccfe3a8532f5fdce0d8e206b7ef33c54e84d
Christian Mamuad <RaphaelTurtlePower@gmail.com>
2014-07-03 00:32:26 -0700

    Added Parse. Tested Connectivity as well

--- src/com/app/letsgo/activities/LoginActivity.java
+++ src/com/app/letsgo/activities/LoginActivity.java
@@ -1,11 +1,12 @@
 package com.app.letsgo.activities;
 
-import com.example.letsgoapp.R;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.letsgoapp.R;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class LoginActivity extends Activity {
 
@@ -13,5 +14,14 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		Parse.initialize(this, "3mZcH7C0jA10fF0esmFovEZh4ZoiGD3stEKhlsLJ", "MyJD5Xqx4jaKzdVzD0DrDt6ZaBqkwTKZcd6mDcqH");
+	
+		/**
+		 * Test Code to see if Parse communicates with the remote server 
+		 */
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+		Toast.makeText(this, "Parse Test Complete", Toast.LENGTH_SHORT).show();
 	}
 }

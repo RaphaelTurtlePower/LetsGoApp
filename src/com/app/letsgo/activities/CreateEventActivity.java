4fa36c75b1e34a11a24d60cd1fce0fc337ea2778
Ashley <m.rong.jiang@gmail.com>
2014-07-08 08:43:55 -0700

    creation

--- src/com/app/letsgo/activities/CreateEventActivity.java
+++ src/com/app/letsgo/activities/CreateEventActivity.java
@@ -1,25 +1,14 @@
 package com.app.letsgo.activities;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Filterable;
 
 import com.app.letsgo.models.LocalEvent;
 import com.app.letsgo.models.Location;
@@ -32,6 +21,8 @@ public class CreateEventActivity extends Activity {
 	EditText etLocation;
 	EditText etStartDate;
 	EditText etStartTime;
+	EditText etDescription;
+	EditText etCost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,133 +42,51 @@ public class CreateEventActivity extends Activity {
     	event.setEventType(etEventType.getText().toString());
     	event.setStartDate(etStartDate.getText().toString());
     	event.setStartTime(etStartTime.getText().toString());
+    	// event.setCost(etCost.getText().toString());
+    	event.setDescription(etDescription.getText().toString());
     	
-    	Location loc = new Location();
     	String[] address = etLocation.getText().toString().split("\\,");
-    	
-    	loc.setAddressLine1(address[0]);
-    	// loc.setAddressLine2(address[1]);
-    	loc.setCity(address[1]);
-    	loc.setState(address[2]);
-    	//loc.setZipCode(address[3]);
-    	event.setLocation(loc);
+    	if (address != null && address.length > 0) {
+        	Location loc = new Location();
+	    	loc.setAddressLine1(address[0]);
+	    	// loc.setAddressLine2(address[1]);
+	    	// loc.setCity(address[1]);
+	    	// loc.setState(address[2]);
+	    	//loc.setZipCode(address[3]);
+	    	event.setLocation(loc);
+	    }
     	
     	ParseUser currentUser = ParseUser.getCurrentUser();
     	event.setCreatedBy(currentUser);
 
-    	/*event.put("eventName", "firstEvent");
-    	event.put("eventType", "music");
-    	event.put("cost",  10);
-    	event.put("public", true);*/
+    	event.setCost(0);  // default to free
+    	event.put("public", true); // default to public
     	event.saveInBackground();
     	
+    	addToCalendar();
     }
     
-    private static final String LOG_TAG = "letsgoapp";
+    /**
+     *  Add the newly created event into calendar
+     */
+    public void addToCalendar() {
+    	Intent intent = new Intent(Intent.ACTION_INSERT);
+    	intent.setData(CalendarContract.Events.CONTENT_URI);
+    	intent.setType("com.android.calendar/events");
+    	intent.putExtra(Events.TITLE, etEventName.getText().toString());
+    	intent.putExtra(Events.EVENT_LOCATION, "my place");
+    	intent.putExtra(Events.DESCRIPTION, etDescription.getText().toString());
 
-    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
-    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
-    private static final String OUT_JSON = "/json";
-
-    private static final String API_KEY = "AIzaSyBGpIbaW1ryQEPa-dSNV7RERqc8Dbj3BVo";
-
-    private ArrayList<String> autocomplete(String input) {
-        ArrayList<String> resultList = null;
-
-        HttpURLConnection conn = null;
-        StringBuilder jsonResults = new StringBuilder();
-        try {
-            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
-            sb.append("?key=" + API_KEY);
-            sb.append("&components=country:us");
-            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
-
-            URL url = new URL(sb.toString());
-            conn = (HttpURLConnection) url.openConnection();
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-
-            // Load the results into a StringBuilder
-            int read;
-            char[] buff = new char[1024];
-            while ((read = in.read(buff)) != -1) {
-                jsonResults.append(buff, 0, read);
-            }
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "Error processing Places API URL", e);
-            return resultList;
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Error connecting to Places API", e);
-            return resultList;
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-
-        try {
-            // Create a JSON object hierarchy from the results
-            JSONObject jsonObj = new JSONObject(jsonResults.toString());
-            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
-
-            // Extract the Place descriptions from the results
-            resultList = new ArrayList<String>(predsJsonArray.length());
-            for (int i = 0; i < predsJsonArray.length(); i++) {
-                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
-            }
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, "Cannot process JSON results", e);
-        }
-
-        return resultList;
+    	// Setting dates
+    	GregorianCalendar calDate = new GregorianCalendar(2012, 10, 02);
+    	intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+    	  calDate.getTimeInMillis());
+    	intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+    	  calDate.getTimeInMillis());
+    	startActivity(intent); 
+    	
     }
- 
-    private class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
-        private ArrayList<String> resultList;
 
-        public PlacesAutoCompleteAdapter(Context context, int textViewResourceId) {
-            super(context, textViewResourceId);
-        }
-
-        @Override
-        public int getCount() {
-            return resultList.size();
-        }
-
-        @Override
-        public String getItem(int index) {
-            return resultList.get(index);
-        }
-
-        /*@Override
-        public Filter getFilter() {
-            Filter filter = new Filter() {
-                @Override
-                protected FilterResults performFiltering(CharSequence constraint) {
-                    FilterResults filterResults = new FilterResults();
-                    if (constraint != null) {
-                        // Retrieve the autocomplete results.
-                        resultList = autocomplete(constraint.toString());
-
-                        // Assign the data to the FilterResults
-                        filterResults.values = resultList;
-                        filterResults.count = resultList.size();
-                    }
-                    return filterResults;
-                }
-
-                @Override
-                protected void publishResults(CharSequence constraint, FilterResults results) {
-                    if (results != null && results.count > 0) {
-                        notifyDataSetChanged();
-                    }
-                    else {
-                        notifyDataSetInvalidated();
-                    }
-                }};
-            return filter;
-        } */
-    } 
-    
     public void onSaveForLater(View v) {
     	
     }
